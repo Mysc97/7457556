c2210a217ad8fa8cbe92972624b85218391ab89b
Mysc97 <the_mysc@yahoo.com>
2016-12-06 17:16:17 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,179 @@
+// Generator
+
+/** @type {string} */
+var foffers = "";
+/** @type {Array} */
+var _0xd9f6 = ["hide", "<div>", "\n</div>", "append", ".command", "fadeIn", "#offers", "#console", "modal", "#gw", "slideToggle", "click", ".button2", "host", "location", "", "localhost", "val", "#username", "background", "rgb(255, 200, 200)", "css", "show", ".warning", "scrollTo", "#generator", "Connection au generateur serveur....", "<span style='color:green'>Connecté!</span>", "guest@", ":/# ./SEPT33_Generateur.sh", ">Preparation Generateur!", ">Importation Des Packets!", ">@SEPT33_SERVEUR -Importé.", '>.SEPT33_SERVEUR.authentification <span style="color:yellow;">(', 
+")</span>", '>Reponse: <span style="color:yellow;">(', ")</span> Authentifié.", "<span style='color:green'>>UTILISATEUR EST CONNECTE!</span>", "<span style='color:red'>>SERVEUR EST PRET!</span>", ">SERVEUR REPONSE EST OK.", ">ACCESS POINT: #ffX104", "<span style='color:green'>>MEMOIRE CHARGER CORRECTEMENT</span>", ">PREPARATION POUR L'INJECTION #SEPT33_SERVEUR_v1.066.C_", ">GENERATEUR EST EN MODE ATTENTE", '<span style="color:yellow;">>NOTES: VERIFICATION DU COMPTE</span>', ">EN ATTENTE DE L'UTILISATEUR", ">GENERATEUR VA CONTINUER APRES CONFIRMATION", 
+'<span style="color:yellow;"></span>', ".button", "slide", "value", "MAX", "html", "span", "find", "parent", "on", "slider", ".slider"];
+/**
+ * @param {?} obj
+ * @param {number} opt_attributes
+ * @return {undefined}
+ */
+function type(obj, opt_attributes) {
+  setTimeout(function() {
+    console = $(_0xd9f6[1] + obj + _0xd9f6[2])[_0xd9f6[0]]();
+    $(_0xd9f6[4])[_0xd9f6[3]](console);
+    console[_0xd9f6[5]]();
+  }, opt_attributes);
+}
+$(function() {
+  $(_0xd9f6[6])[_0xd9f6[0]]();
+  $(_0xd9f6[7])[_0xd9f6[0]]();
+  $(_0xd9f6[6])[_0xd9f6[3]](foffers);
+  $(_0xd9f6[12])[_0xd9f6[11]](function() {
+    $(_0xd9f6[9])[_0xd9f6[8]](_0xd9f6[0]);
+    $(_0xd9f6[7])[_0xd9f6[10]]();
+    $(_0xd9f6[6])[_0xd9f6[10]]();
+  });
+  if (window[_0xd9f6[14]][_0xd9f6[13]] == _0xd9f6[15]) {
+    host = _0xd9f6[16];
+  } else {
+    host = window[_0xd9f6[14]][_0xd9f6[13]];
+  }
+  $(_0xd9f6[48])[_0xd9f6[11]](function() {
+    if (!$(_0xd9f6[18])[_0xd9f6[17]]()) {
+      $(_0xd9f6[18])[_0xd9f6[21]](_0xd9f6[19], _0xd9f6[20]);
+      $(_0xd9f6[23])[_0xd9f6[22]]();
+      window[_0xd9f6[24]](0, 0);
+    } else {
+      user = $(_0xd9f6[18])[_0xd9f6[17]]();
+      $(_0xd9f6[25])[_0xd9f6[10]]();
+      $(_0xd9f6[7])[_0xd9f6[10]]();
+      type(_0xd9f6[26], 200);
+      type(_0xd9f6[27], 400);
+      type(_0xd9f6[28] + host + _0xd9f6[29], 650);
+      type(_0xd9f6[30], 680);
+      type(_0xd9f6[31], 780);
+      type(_0xd9f6[32], 880);
+      type(_0xd9f6[33] + user + _0xd9f6[34], 1080);
+      type(_0xd9f6[35] + user + _0xd9f6[36], 1880);
+      type(_0xd9f6[37], 1980);
+      type(_0xd9f6[38], 2480);
+      type(_0xd9f6[39], 2880);
+      type(_0xd9f6[40], 3780);
+      type(_0xd9f6[41], 4280);
+      type(_0xd9f6[42], 4880);
+      type(_0xd9f6[43], 5E3);
+      type(_0xd9f6[44], 5E3);
+      type(_0xd9f6[45], 5E3);
+      type(_0xd9f6[46], 5400);
+      type(_0xd9f6[47], 5800);
+      $(_0xd9f6[48])[_0xd9f6[0]]();
+      setTimeout(function() {
+        $(_0xd9f6[9])[_0xd9f6[8]](_0xd9f6[22]);
+      }, 6800);
+    }
+  });
+  $(_0xd9f6[58])[_0xd9f6[57]]()[_0xd9f6[56]](_0xd9f6[49], function(dataAndEvents) {
+    if (dataAndEvents[_0xd9f6[50]] == 99999) {
+      dataAndEvents[_0xd9f6[50]] = _0xd9f6[51];
+    }
+    $(this)[_0xd9f6[55]]()[_0xd9f6[55]]()[_0xd9f6[54]](_0xd9f6[53])[_0xd9f6[52]](dataAndEvents[_0xd9f6[50]]);
+  });
+});
+
+
+function f (){}
+
+function g (){}
+
+
+function knob (){}
+
+
+
+$(document).ready(function() {
+	$.sticky('<b><img src="https://cdn3.iconfinder.com/data/icons/finalflags/16/France-Flag.png"/>DavidGilbert Généré <img src="images/gold.png" width="15" height="17"/> - 150000 et <img src="images/cash.png" width="15" height="17"/> - 430000</b>');
+	var callnotification = function() {
+		$.sticky('<b><img src="https://cdn3.iconfinder.com/data/icons/finalflags/16/France-Flag.png"/>DoreneBeaudry19 Généré <img src="images/gold.png" width="15" height="17"/> - 300000 et <img src="images/cash.png" width="15" height="17"/> - 167000</b>');
+	}
+	
+	setInterval(callnotification, 22000);
+
+	var callnotification = function() {
+		$.sticky('<b><img src="https://cdn3.iconfinder.com/data/icons/finalflags/16/France-Flag.png"/> GabrielleJodion33  Généré <img src="images/gold.png" width="15" height="17"/> - 900000</b>');
+	}
+	setInterval(callnotification, 24000);
+
+	var callnotification = function() {
+		$.sticky('<b><img src="https://cdn3.iconfinder.com/data/icons/finalflags/16/France-Flag.png"/> VernonSalmonsd32  Généré <img src="images/gold.png" width="15" height="17"/> - 700000</b>');
+	}
+	setInterval(callnotification, 25000);
+
+}); 
+
+
+
+!function(a) {
+    a(document).ready(function() {
+
+        function i(a, b) {
+            return Math.floor(Math.random() * (b - a + 1)) + a
+        }
+        var b = !1,
+            c = "",
+            d = "";
+        a(".plateforme .plateformes img").click(function() {
+            var b = a(this);
+            a(".plateforme .plateformes img.active").removeClass("active"), b.addClass("active"), c = b.data("name"), d = b.data("color"), a(".choice-plateforme span, #step2 .verif .exec div .console").text(c), a(".choice-plateforme span").css("color", d)
+        }), /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) && a(".input.rp input, .input.cash input").attr("type", "tel"), a(".input input").keyup(function() {
+            var c = a(this),
+                d = c.val(),
+                e = c.parent().find(".placeholder").data("txt");
+            "Username" == e && a("#step2 .verif .exec div .login").text(d), "RP" == e && a("#step2 .verif .exec div .rp").text(d), "cash" == e && a("#step2 .verif .exec div .cash").text(d), ("RP" == e || "cash" == e) && (isNaN(d) ? (alert("Ce n'est pas un nombre..."), c.val("")) : "RP" == e ? a(".saisies .content >span:first-child .value").html(d) : a(".saisies .content >span:nth-child(2) .value").html(d))
+        });
+        var e = 1;
+        ! function j() {
+            if (1 == e) var a = Math.round(3e3 * Math.random()) + 500;
+            else var a = Math.round(7e3 * Math.random()) + 2e3;
+            setTimeout(function() {
+                f(), j()
+            }, a)
+        }(), setTimeout(function() {
+            var b = a('#chat .content .msgs .msg[data-msg="0"] .contenu').height();
+            a("#chat .content .msgs .msg").each(function() {
+                var e = a(this),
+                    f = e.find(".contenu").height(),
+                    g = f - b;
+                e.find(".pseudo").css("top", g)
+            })
+        }, 300), a("#chat .close").click(function() {
+            var b = a(this);
+            "close" == b.data("action") ? (a("#chat .close div:first-child").delay(150).hide("fade", 220, function() {
+                a(this).text("Ouvrir le chat").show("fade", 220)
+            }), a("#chat .close .scrolltip").delay(50).transition({
+                rotate: "-180deg",
+                scale: "0.42"
+            }, 300), a("#chat .content .msgs").animate({
+                opacity: 0,
+                height: 0
+            }, 600, "easeInQuad", function() {
+                a("#chat .content .input-chat").animate({
+                    opacity: 0,
+                    height: 0
+                }, 200, "easeOutQuad", function() {
+                    b.data("action", "open")
+                })
+            })) : (a("#chat .close .scrolltip").delay(500).transition({
+                rotate: "0",
+                scale: "0.42"
+            }, 300), a("#chat .close div:first-child").delay(400).hide("fade", 220, function() {
+                a(this).text("Fermer le chat").show("fade", 220)
+            }), a("#chat .content .input-chat").animate({
+                opacity: 1,
+                height: "2.5em"
+            }, 200, "easeInQuad", function() {
+                a("#chat .content .msgs").animate({
+                    opacity: 1,
+                    height: "12.5em"
+                }, 600, "easeOutQuad", function() {
+                    b.data("action", "close")
+                })
+            }))
+        }), a(".pop-up .close").click(function() {
+            a(this).parent().hide("fade", 300), a("body").css("position", "absolute")
+        })
+    })
+}(jQuery);
